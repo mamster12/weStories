3bdde8cba03ebfbd6d7886c2f362e0defd4c230a
unknown <mcvilleanrapsody@gmail.com>
2020-02-24 17:46:47 +0800

    handled auth

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//load router
+const auth = require('./routes/auth');
+
+//load config
+require('./config/passport')(passport);
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('HomePage');
 });
+
+//use the routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 3000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://macvillegas3:qwerty12345mac@cluster0-dctwh.mongodb.net/weStories-dev?retryWrites=true&w=majority',
+    googleClientID: '235220927963-jbomrfd9631vi0k98b21t6mllum3mbip.apps.googleusercontent.com',
+    googleClientSecret: 'TrUgyBmNRfUIn53lQcSKsxSm'
+}
