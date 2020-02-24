7d2e7187b3327cace0f1f65b88be18ab5497d0a4
unknown <mcvilleanrapsody@gmail.com>
2020-02-24 18:32:52 +0800

    add new scope to auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
+// router.get('/google',
+//     passport.authenticate('google', { scope: ['profile', 'email'] }));
+
 router.get('/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] }));
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
 
 router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }),
